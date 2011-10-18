71ddb51239da030f950071045372d110111f5be5
Jacky Chan <linux_china@hotmail.com>
2011-10-18 11:43:00 +0800

    设置路径

--- src/test/java/org/lilystudio/smarty4j/SmartyTemplateTest.java
+++ src/test/java/org/lilystudio/smarty4j/SmartyTemplateTest.java
@@ -25,6 +25,7 @@ public class SmartyTemplateTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         Map<String, String> config = new HashMap<String, String>();
+        config.put("template.path", "src/test/resources/templates/");
         this.smartyEngine = new Engine(config);
     }
 
@@ -34,9 +35,9 @@ public class SmartyTemplateTest extends TestCase {
      * @throws Exception exception
      */
     public void testRender() throws Exception {
-        Template template = smartyEngine.parseTemplate("<title>{$title}</title>");
+        Template template = smartyEngine.getTemplate("demo.tpl");
         Context context = new Context();
-        context.set("title", "欢迎光临");
+        context.set("title", "欢迎光临222");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         template.merge(context, out);
         System.out.println(out.toString("utf-8"));
