33cadb407b415670a23b40b03bbf1009fb735e16
Jacky Chan <linux_china@hotmail.com>
2011-10-19 11:37:57 +0800

    minor update

--- src/test/java/org/lilystudio/smarty4j/SmartyTemplateTest.java
+++ src/test/java/org/lilystudio/smarty4j/SmartyTemplateTest.java
@@ -3,7 +3,9 @@ package org.lilystudio.smarty4j;
 import junit.framework.TestCase;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,6 +28,7 @@ public class SmartyTemplateTest extends TestCase {
         super.setUp();
         Map<String, String> config = new HashMap<String, String>();
         config.put("template.path", "src/test/resources/templates/");
+        config.put("encoding", "GBK");
         this.smartyEngine = new Engine(config);
     }
 
@@ -38,8 +41,12 @@ public class SmartyTemplateTest extends TestCase {
         Template template = smartyEngine.getTemplate("demo.tpl");
         Context context = new Context();
         context.set("title", "欢迎光临222");
+        List<User> users = new ArrayList<User>();
+        users.add(new User(1, "马克"));
+        users.add(new User(2, "马克"));
+        context.set("users", users);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         template.merge(context, out);
-        System.out.println(out.toString("utf-8"));
+        System.out.println(out.toString("GBK"));
     }
 }
