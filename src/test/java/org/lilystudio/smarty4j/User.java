db7e0ad6fecffd291bba56d3cd59606483c33de7
Jacky Chan <linux_china@hotmail.com>
2011-10-19 10:58:44 +0800

    user

---
+++ src/test/java/org/lilystudio/smarty4j/User.java
@@ -0,0 +1,36 @@
+package org.lilystudio.smarty4j;
+
+/**
+ * user
+ *
+ * @author linux_china
+ */
+public class User {
+    private Integer id;
+    private String name;
+
+    public User() {
+
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
